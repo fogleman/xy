1ea8f2e3ebe9308b9bc1d8d1314bd6b415f75fa1
Michael Fogleman <fogleman@gmail.com>
2015-12-29 21:16:10 -0500

    sort_paths can reverse paths too

--- xy/planner.py
+++ xy/planner.py
@@ -2,11 +2,14 @@ from math import hypot
 import anneal
 import random
 
-def sort_paths(paths, iterations=100000):
+def sort_paths(paths, iterations=100000, reversable=True):
     '''
     This function re-orders a set of 2D paths (polylines) to minimize the
     distance required to visit each path. This is useful for 2D plotting to
     reduce wasted movements where the instrument is not drawing.
+
+    If allowed, the algorithm will also reverse some paths if doing so reduces
+    the total distance.
 
     The code uses simulated annealing as its optimization algorithm. The number
     of iterations can be increased to improve the chances of finding a perfect
@@ -16,10 +19,13 @@ def sort_paths(paths, iterations=100000):
     With randomly generated paths, the algorithm can quickly find a solution
     that reduces the extra distance to ~25 percent of its original value.
     '''
-    state = Model(paths)
+    state = Model(list(paths), reversable)
     max_temp = anneal.get_max_temp(state, 10000)
     min_temp = max_temp / 1000.0
     state = anneal.anneal(state, max_temp, min_temp, iterations)
+    for path, reverse in zip(state.paths, state.reverse):
+        if reverse:
+            path.reverse()
     return state.paths
 
 def sort_points(points, iterations=100000):
@@ -28,13 +34,15 @@ def sort_points(points, iterations=100000):
     This is basically a traveling salesman optimization.
     '''
     paths = [[x] for x in points]
-    paths = sort_paths(paths, iterations)
+    paths = sort_paths(paths, iterations, False)
     points = [x[0] for x in paths]
     return points
 
 class Model(object):
-    def __init__(self, paths, distances=None, total_distance=None):
+    def __init__(self, paths, reversable=True, reverse=None, distances=None, total_distance=None):
         self.paths = paths
+        self.reversable = reversable
+        self.reverse = reverse or [False] * len(self.paths)
         if distances:
             self.total_distance = total_distance or 0
             self.distances = distances
@@ -50,35 +58,60 @@ class Model(object):
     def add_distances(self, indexes):
         n = len(self.distances)
         for i in indexes:
-            if i >= 0 and i < n:
+            if i < 0 or i >= n:
+                continue
+            j = i + 1
+            if self.reverse[i]:
+                x1, y1 = self.paths[i][0]
+            else:
                 x1, y1 = self.paths[i][-1]
-                x2, y2 = self.paths[i + 1][0]
-                self.distances[i] = hypot(x2 - x1, y2 - y1)
-                self.total_distance += self.distances[i]
+            if self.reverse[j]:
+                x2, y2 = self.paths[j][-1]
+            else:
+                x2, y2 = self.paths[j][0]
+            self.distances[i] = hypot(x2 - x1, y2 - y1)
+            self.total_distance += self.distances[i]
     def energy(self):
         # return the total extra distance for this ordering
         return self.total_distance
     def do_move(self):
-        # mutate the state by swapping two random paths
-        n = len(self.paths) - 1
-        i = random.randint(0, n)
-        j = random.randint(0, n)
-        indexes = set([i - 1, i, j - 1, j])
-        self.subtract_distances(indexes)
-        self.paths[i], self.paths[j] = self.paths[j], self.paths[i]
-        self.add_distances(indexes)
-        return (i, j)
+        if self.reversable and random.random() < 0.25:
+            # mutate by reversing a random path
+            n = len(self.paths) - 1
+            i = random.randint(0, n)
+            indexes = [i - 1, i]
+            self.subtract_distances(indexes)
+            self.reverse[i] = not self.reverse[i]
+            self.add_distances(indexes)
+            return (1, i, 0)
+        else:
+            # mutate by swapping two random paths
+            n = len(self.paths) - 1
+            i = random.randint(0, n)
+            j = random.randint(0, n)
+            indexes = set([i - 1, i, j - 1, j])
+            self.subtract_distances(indexes)
+            self.paths[i], self.paths[j] = self.paths[j], self.paths[i]
+            self.add_distances(indexes)
+            return (0, i, j)
     def undo_move(self, undo):
         # undo the previous mutation
-        i, j = undo
-        indexes = set([i - 1, i, j - 1, j])
-        self.subtract_distances(indexes)
-        self.paths[i], self.paths[j] = self.paths[j], self.paths[i]
-        self.add_distances(indexes)
+        mode, i, j = undo
+        if mode == 0:
+            indexes = set([i - 1, i, j - 1, j])
+            self.subtract_distances(indexes)
+            self.paths[i], self.paths[j] = self.paths[j], self.paths[i]
+            self.add_distances(indexes)
+        else:
+            indexes = [i - 1, i]
+            self.subtract_distances(indexes)
+            self.reverse[i] = not self.reverse[i]
+            self.add_distances(indexes)
     def copy(self):
         # make a copy of the model
         return Model(
-            list(self.paths), list(self.distances), self.total_distance)
+            list(self.paths), self.reversable, list(self.reverse),
+            list(self.distances), self.total_distance)
 
 def test(n_paths, n_iterations, seed=None):
     random.seed(seed)
