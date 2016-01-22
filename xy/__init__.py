8326316806e3a1e335a3bc07b6ba445440790ad7
Michael Fogleman <fogleman@gmail.com>
2016-01-22 17:37:21 -0500

    arc

--- xy/__init__.py
+++ xy/__init__.py
@@ -6,7 +6,7 @@ from gcode import GCode
 
 from planner import sort_paths, sort_paths_greedy, sort_points
 
-from primitives import circle
+from primitives import circle, arc
 
 from turtles import Turtle
 
--- xy/primitives.py
+++ xy/primitives.py
@@ -9,3 +9,13 @@ def circle(x=0, y=0, r=1, n=36):
         py = y + math.sin(a) * r
         result.append((px, py))
     return result
+
+def arc(x=0, y=0, r=1, a1=0, a2=360, n=36):
+    result = []
+    for i in range(n + 1):
+        p = i / float(n)
+        a = math.radians(a1 + (a2 - a1) * p)
+        px = x + math.cos(a) * r
+        py = y + math.sin(a) * r
+        result.append((px, py))
+    return result
